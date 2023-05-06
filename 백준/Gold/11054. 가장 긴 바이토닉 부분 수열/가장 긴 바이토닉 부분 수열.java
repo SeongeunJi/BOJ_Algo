0cb1dfeef616e0e29595df0510ebe5b4ff3bbfc5
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-05-06 23:48:15 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 168 ms, Memory: 14960 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,38 +1,39 @@
 import java.io.*;
 import java.util.StringTokenizer;
-public class Main {
+ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] A = new int[n+1];
-        int[][] dp = new int[n+1][3];
+        int t = Integer.parseInt(br.readLine());
+        int[] A = new int[t + 1];
+        int[][] dp = new int[t + 1][2];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 1; i <= n; i++)
+        for (int i = 1; i <= t; i++)
             A[i] = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= n; i++) {
-            dp[i][1] = 1;
-            for(int j = 1; j < i; j++) {
-                if(A[i] > A[j] && dp[i][1] <= dp[j][1])
+        // get LIS
+        for (int i = 1; i <= t; i++) {
+            for (int j = 1; j < i; j++) {
+                if (A[i] > A[j] && dp[i][0] <= dp[j][0])
+                    dp[i][0] = dp[j][0] + 1;
+            }
+        }
+
+        for (int i = t; i >= 1; i--) {
+            for (int j = t; j > i; j--) {
+                if (A[i] > A[j] && dp[i][1] <= dp[j][1])
                     dp[i][1] = dp[j][1] + 1;
             }
         }
 
-        for(int i = n; i >= 1; i--) {
-            dp[i][2] = 1;
-            for(int j = n; j > i; j--)
-                if(A[i] > A[j] && dp[i][2] <= dp[j][2])
-                    dp[i][2] = dp[j][2] + 1;
-        }
+        int result = 0;
+        for(int i = 1; i <= t; i++)
+            result = Math.max(result, dp[i][1] + dp[i][0] + 1);
 
-        int max = 0;
-        for(int i = 1; i <= n; i++)
-            max = Math.max(dp[i][1] + dp[i][2] - 1, max);
-
-        bw.write(String.valueOf(max));
+        bw.write(String.valueOf(result));
         bw.flush();
+        bw.close();
     }
 }
