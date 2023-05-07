3c0d87ec018cf15cab00465c47a8b36c0401fd4c
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-05-07 12:07:44 +0900

    [Gold V] Title: 연속합 2, Time: 288 ms, Memory: 26112 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -10,25 +10,30 @@ public class Main {
         int[] A = new int[n+1];
         int[][] dp = new int[n+1][3];
 
+
+        // 입력값 초기화
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 1; i <= n; i++)
             A[i] = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= n; i++)
-            dp[i][1] = Math.max(dp[i-1][1] + A[i], A[i]);
-
-        dp[n][2] = A[n];
-        int max = dp[n][1];
-        for(int i = n-1; i >= 1; i--) {
-            dp[i][2] = Math.max(dp[i+1][2] + A[i], A[i]);
-            max = Math.max(max, dp[i][1]);
+        // i번째로 끝나는 최대 연속합 + 최댓값 한번에 구하기
+        int max = A[1];
+        for(int i = 1; i <= n; i++) {
+            dp[i][1] = Math.max(A[i], A[i] + dp[i - 1][1]);
+            max = Math.max(dp[i][1], max);
         }
 
+        // i번째로 시작하는 최대 연속합
+        dp[n][2] = A[n];
+        for(int i = n-1; i >= 1; i--)
+            dp[i][2] = Math.max(A[i], A[i] + dp[i+1][2]);
+
+        // 제거한 모든 경우 vs dp[i][1]
         for(int i = 1; i <= n; i++) {
             if(i == n) continue;
-            max = Math.max(dp[i-1][1] + dp[i+1][2], max);
+            max = Math.max(max, dp[i - 1][1] + dp[i + 1][2]);
         }
-        
+
         bw.write(String.valueOf(max));
         bw.flush();
         bw.close();
