a31b63e2892bf07a72a927fbbac49415c4f37760
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-05-10 14:29:11 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 128 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int len = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= len; i++) {
+            for(int j = i; j <= len; j++)
+                bw.write("*");
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
