4875990a19c84f269ccd36d8001ff82befbe70bb
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-05-10 13:35:20 +0900

    [Bronze II] Title: 숫자, Time: 148 ms, Memory: 16028 KB -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[2];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 2; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        int max = Math.max(arr[1], arr[0]);
+        int min = arr[0] + arr[1] - max;
+
+        bw.write((max == min ? 0 : max - min -1) + "\n");
+        if (max != min) {
+            for(int i = min + 1; i < max; i++)
+                bw.write(i + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
