90e29224c32b44c5396055e068b52540b11843d3
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-05-11 12:12:22 +0900

    [Bronze II] Title: Strfry, Time: 316 ms, Memory: 22692 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -1,42 +1,26 @@
-
 import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        List<Character> chList = new LinkedList<>();
         while (N-- > 0) {
+            int[] arr = new int[26];
             StringTokenizer st = new StringTokenizer(br.readLine());
-            char[] chars = st.nextToken().toCharArray();
-            char[] chars2 = st.nextToken().toCharArray();
+            String str1 = st.nextToken();
+            String str2 = st.nextToken();
 
-            if (chars.length != chars2.length) {
-                bw.write("Impossible\n");
-                continue;
-            }
+            for(char ch : str1.toCharArray())
+                arr[ch-'a']++;
+            for(char ch : str2.toCharArray())
+                arr[ch-'a']--;
 
-            for(char ch : chars)
-                chList.add(ch);
-            boolean check = true;
-
-            for (char ch : chars2) {
-                if (chList.contains(ch)) {
-                    Character tmp = ch;
-                    chList.remove(tmp);
-                } else {
-                    bw.write("Impossible\n");
-                    check = false;
-                    break;
-                }
-            }
+            boolean check = Arrays.stream(arr).allMatch(E -> E == 0);
             if(check) bw.write("Possible\n");
-            chList.clear();
+            if(!check) bw.write("Impossible\n");
         }
         bw.flush();
         bw.close();
