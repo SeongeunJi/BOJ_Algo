3c76aceb7bab63a3c6c5e41366b9ddc4f77a168b
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-05-20 19:43:15 +0900

    [Silver I] Title: 숨바꼭질, Time: 168 ms, Memory: 20496 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -9,31 +9,19 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int size = 100000;
-        int[] board = new int[size+1];
-        int[] dx = {-1, 1};
-
+        int[] board = new int[100001];
         Arrays.fill(board, -1);
 
         Queue<Integer> Q = new ArrayDeque<>();
         Q.add(N); board[N] = 0;
-        loop1:
-        while (!Q.isEmpty()) {
-            int cur = Q.remove();
-            for (int i = 0; i < 2; i++) {
-                int nx = cur + dx[i];
-                if(nx < 0 || nx > size || board[nx] >= 0) continue;
+        while (board[K] == -1) {
+            int curX = Q.remove();
+            for (int nx : new int[]{curX + 1, curX - 1, curX * 2}) {
+                if(nx < 0 || nx > 100000 || board[nx] >= 0) continue;
                 Q.add(nx);
-                board[nx] = board[cur] + 1;
-                if(nx == K) break loop1;
+                board[nx] = board[curX] + 1;
             }
-            int nx = cur * 2;
-            if(nx > size || board[nx] >= 0) continue;
-            Q.add(nx);
-            board[nx] = board[cur] + 1;
-            if(nx == K) break;
         }
-
         bw.write(String.valueOf(board[K]));
         bw.flush();
         bw.close();
