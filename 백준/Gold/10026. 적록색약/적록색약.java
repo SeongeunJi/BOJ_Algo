04992e72ca8353f694125562eb556d4c32117500
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-05-23 16:42:52 +0900

    [Gold V] Title: 적록색약, Time: 168 ms, Memory: 16588 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.ArrayDeque;
 import java.util.Queue;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,25 +8,22 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         String[] board = new String[N];
-        boolean[][] vis1 = new boolean[N][N];
-        boolean[][] vis2 = new boolean[N][N];
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, 1, 0, -1};
 
         for (int i = 0; i < N; i++)
             board[i] = br.readLine();
 
-        int cnt1 = 0;
-        int cnt2 = 0;
         Queue<Pair<Integer>> q = new ArrayDeque<>();
-        for(int f = 0; f < 2; f++) {
+        for(int h = 0; h < 2; h++) {
+            boolean[][] vis = new boolean[N][N];
+            int cnt = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (vis1[i][j]) continue;
-                    char first = board[i].charAt(j);
+                    if(vis[i][j]) continue;
+                    cnt++;
                     q.add(new Pair<>(i, j));
-                    vis1[i][j] = true;
-                    cnt1++;
+                    char first = board[i].charAt(j);
                     while (!q.isEmpty()) {
                         Pair<Integer> cur = q.remove();
                         for (int k = 0; k < 4; k++) {
@@ -35,40 +31,21 @@ public class Main {
                             int ny = cur.y + dy[k];
 
                             if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                            if (vis1[nx][ny]) continue;
-                            if(f == 0 && board[nx].charAt(ny) != first) continue;
+                            if (vis[nx][ny]) continue;
+                            if (h == 0) {
+                                if (board[nx].charAt(ny) != first) continue;
+                            } else {
+                                if((first == 'R' || first == 'G') && board[nx].charAt(ny) == 'B') continue;
+                                else if (first == 'B' && board[nx].charAt(ny) != 'B') continue;
+                            }
                             q.add(new Pair<>(nx, ny));
-                            vis1[nx][ny] = true;
+                            vis[nx][ny] = true;
                         }
                     }
                 }
             }
+            bw.write(cnt + " ");
         }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if(vis2[i][j]) continue;
-                char first = board[i].charAt(j);
-                q.add(new Pair<>(i, j));
-                vis2[i][j] = true;
-                cnt2++;
-                while (!q.isEmpty()) {
-                    Pair<Integer> cur = q.remove();
-                    for (int k = 0; k < 4; k++) {
-                        int nx = cur.x + dx[k];
-                        int ny = cur.y + dy[k];
-
-                        if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                        if(vis2[nx][ny]) continue;
-                        if((first == 'R' || first == 'G') && board[nx].charAt(ny) == 'B') continue;
-                        else if (first == 'B' && board[nx].charAt(ny) != 'B') continue;
-                        q.add(new Pair<>(nx, ny));
-                        vis2[nx][ny] = true;
-                    }
-                }
-            }
-        }
-        bw.write(cnt1 + " " + cnt2);
         bw.flush();
         bw.close();
     }
