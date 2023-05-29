b7d7132e20b17308fbc4dee2aabcdcd63dc5e191
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-05-29 10:42:12 +0900

    [Gold II] Title: 확장 게임, Time: 724 ms, Memory: 75980 KB -BaekjoonHub

--- 백준/Gold/16920. 확장 게임/확장 게임.java
+++ 백준/Gold/16920. 확장 게임/확장 게임.java
@@ -1,82 +1,81 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 public class Main {
+    static int[][] board;
+    static int[] pi;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    static int x, y, p;
+    static int[] result;
+    static final int MAX = 1000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
-
-        int MAX = 1000000;
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
-
-        int[][] board = new int[x][y];
-        int[] pi = new int[p+1];
-        int[] result = new int[p+1];
-        List<Queue<Pair>> lists = new LinkedList<>();
-
-        for(int i = 0; i <= p; i++)
-            lists.add(new LinkedList<>());
-
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        p = Integer.parseInt(st.nextToken());
+        board = new int[x][y];
+        pi = new int[p+1];
+        result = new int[p+1];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= p; i++) {
+        for(int i=1; i<=p; i++) {
             pi[i] = Integer.parseInt(st.nextToken());
-            if(pi[i] > MAX) pi[i] = MAX;
+            if(pi[i]>MAX) pi[i] = MAX;
         }
-
-        for (int i = 0; i < x; i++) {
-            String input = br.readLine();
-            for (int j = 0; j < y; j++) {
-                char ch = input.charAt(j);
+        LinkedList<Pair>[] list = new LinkedList[p+1];
+        for(int i=1; i<=p; i++) list[i] = new LinkedList<>();
+        for(int i = 0; i< x; i++) {
+            String in = br.readLine();
+            for(int j = 0; j< y; j++) {
+                char ch = in.charAt(j);
                 if(ch == '#') board[i][j] = -1;
                 else if(ch == '.') board[i][j] = 0;
                 else {
-                    board[i][j] = ch - '0';
-                    lists.get(board[i][j]).add(new Pair(i, j));
+                    board[i][j] = ch-'0';
+                    list[board[i][j]].add(new Pair(i,j));
                     result[board[i][j]]++;
                 }
             }
         }
-
         boolean finish = false;
-        while (!finish) {
-            finish = true;
-            for (int idx = 1; idx <= p; idx++) {
-                int exCnt = 0;
-                while (!lists.get(idx).isEmpty()) {
-                    int size = lists.get(idx).size();
-                    while(size --> 0) {
-                        Pair cur = lists.get(idx).poll();
-                        for (int dir = 0; dir < 4; dir++) {
+        while(!finish) {
+            for(int i=1; i<=p; i++) {
+                int cnt = 0;
+                while(!list[i].isEmpty()) {
+                    int castleCnt = list[i].size();
+                    while(castleCnt-- > 0) {
+                        Pair cur = list[i].poll();
+                        for(int dir=0; dir<4; dir++) {
                             int nx = cur.x + dx[dir];
                             int ny = cur.y + dy[dir];
-                            if(nx < 0 || ny < 0 || nx >= x || ny >= y) continue;
-                            if(board[nx][ny] != 0) continue;
-                            lists.get(idx).add(new Pair(nx, ny));
-                            board[nx][ny] = idx;
-                            result[idx]++;
+                            if(OOB(nx, ny) || board[nx][ny] != 0) continue;
+                            result[i]++;
+                            list[i].add(new Pair(nx,ny));
+                            board[nx][ny] = i;
                         }
                     }
-                    if(++exCnt == pi[idx]) break;
+                    if(++cnt == pi[i]) break;
                 }
             }
-            for (Queue<Pair> q : lists) {
-                if (!q.isEmpty()) {
+            finish = true;
+            for(int i=1; i<=p; i++) {
+                if (!list[i].isEmpty()) {
                     finish = false;
                     break;
                 }
             }
         }
-        for(int i = 1; i <= p; i++)
+        for(int i=1; i<=p; i++)
             bw.write(result[i] + " ");
         bw.flush();
         bw.close();
     }
-    static class Pair {
+    private static boolean OOB(int nx, int ny) {
+        return nx < 0 || nx >= x || ny < 0 || ny >= y;
+    }
+    static class Pair{
         int x, y;
         public Pair(int x, int y) {
             this.x = x;
