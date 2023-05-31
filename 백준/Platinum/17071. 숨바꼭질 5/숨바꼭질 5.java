c17aad35eb188aa34e290344d1553fee6cb38f00
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-05-31 09:14:06 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 272 ms, Memory: 82392 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.java
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.java
@@ -1,34 +1,32 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 public class Main {
-    static int n, k;
-    static boolean[][] vis;
-    static Queue<Integer> q = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        vis = new boolean[2][500001];
-        bw.write(String.valueOf((n == k) ? 0 : bfs()));
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf((n == k) ? 0 : bfs(n, k)));
         bw.flush();
         bw.close();
     }
-    private static int bfs() {
-        q.add(n);
-        vis[0][n] = true;
+    private static int bfs(int n, int k) {
+        boolean[][] vis = new boolean[2][500001];
+        Queue<Integer> q = new LinkedList<>();
+        vis[0][n] = true; q.add(n);
+
         int time = 0;
-        
         while (!q.isEmpty()) {
             int mod = ++time % 2;
             k += time;
             if(k > 500000) return -1;
-            int size = q.size();
-            while (size-- > 0) {
+            int sz = q.size();
+            while(sz --> 0) {
                 int cx = q.remove();
                 for (int nx : new int[]{cx + 1, cx - 1, cx * 2}) {
-                    if (nx < 0 || nx > 500000 || vis[mod][nx]) continue;
+                    if(nx < 0 || nx > 500000 || vis[mod][nx]) continue;
                     q.add(nx);
                     vis[mod][nx] = true;
                 }
