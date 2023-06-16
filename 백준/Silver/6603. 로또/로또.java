0f7c59eafcb16043f4126a7ad9dae6a8e0571698
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-06-17 06:06:01 +0900

    [Silver II] Title: 로또, Time: 144 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -2,26 +2,22 @@ import java.io.*;
 import java.util.*;
 public class Main {
     static StringBuilder sb = new StringBuilder();
-    static StringBuilder sb2 = new StringBuilder();
     static List<Integer> list = new ArrayList<>();
     static int M = 6;
     static int N;
-    static int[] arr = new int[6];
-    static boolean[] vis;
+    static int[] arr = new int[M];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while(true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
-            vis = new boolean[N];
             if(N == 0) break;
             while(st.hasMoreTokens()) list.add(Integer.parseInt(st.nextToken()));
             go(0, 0);
-            sb2.append(sb).append("\n");
-            sb = new StringBuilder();
+            sb.append("\n");
             list.clear();
         }
-        System.out.println(sb2);
+        System.out.println(sb);
     }
 
     static void go(int level, int start) {
@@ -31,11 +27,8 @@ public class Main {
             return;
         }
         for (int i = start; i < N; i++) {
-            if(vis[i]) continue;
             arr[level] = list.get(i);
-            vis[i] = true;
-            go(level + 1, i);
-            vis[i] = false;
+            go(level + 1, i + 1);
         }
     }
 }
