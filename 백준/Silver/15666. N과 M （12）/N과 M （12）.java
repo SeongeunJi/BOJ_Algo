a3e252e19a77c908bd846378a5710cf43e2d8248
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-06-16 15:37:21 +0900

    [Silver II] Title: N과 M (12), Time: 172 ms, Memory: 17484 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int N, M;
+    static List<Integer> list = new ArrayList<>();
+    static Set<String> lhs = new LinkedHashSet<>();
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr = new int[10];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()) list.add(Integer.parseInt(st.nextToken()));
+        Collections.sort(list);
+        go(0, 0);
+        for(String str : lhs) sb.append(str).append("\n");
+        System.out.println(sb);
+    }
+
+    static void go(int level, int start) {
+        if (level == M) {
+            for (int i = 0; i < M; i++) sb.append(arr[i]).append(" ");
+            lhs.add(sb.toString());
+            sb = new StringBuilder();
+            return;
+        }
+        for (int i = start; i < N; i++) {
+            if(list.get(start) > list.get(i)) continue;
+            arr[level] = list.get(i);
+            go(level + 1, i);
+        }
+    }
+}
