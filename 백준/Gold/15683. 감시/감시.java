9593794a0a184bc4f96ba9731fe8269b46531161
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-06-22 10:03:11 +0900

    [Gold IV] Title: 감시, Time: 292 ms, Memory: 99864 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,88 +1,102 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 public class Main {
-    static int X, Y, blindSpots;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static List<Pair> cctvList = new ArrayList<>();
-    static int[][] board1;
-    static int[][] board2;
+    static int[][] dirArr = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; // clockwise
+    static final int NORTH = 0, EAST = 1, SOUTH = 2, WEST = 3;
+    static int[] arr;
+    static int x, y;
+    static int[][] board;
+    static List<Integer> cctvList = new ArrayList<>();
+    static int blindSpots, cctvCnt, ans = Integer.MAX_VALUE;
+    static Queue<Pair> q = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        X = Integer.parseInt(st.nextToken());
-        Y = Integer.parseInt(st.nextToken());
-        board1 = new int[X][Y];
-        board2 = new int[X][Y];
-        for (int i = 0; i < X; i++) {
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        board = new int[x][y];
+        for (int i = 0; i < x; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < Y; j++) {
-                board1[i][j] = Integer.parseInt(st.nextToken());
-                if(board1[i][j] >= 1 && board1[i][j] <= 5)
-                    cctvList.add(new Pair(i, j));
-                else if(board1[i][j] == 0) blindSpots++;
+            for(int j = 0; j < y; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if(board[i][j] == 0) blindSpots++;
+                else if(board[i][j] != 6) {
+                    cctvList.add(i * y + j);
+                    cctvCnt++;
+                }
             }
         }
-        go();
+        arr = new int[cctvCnt];
+        go(0, 0);
+        System.out.println(ans);
     }
-
-    static void go() {
-        for(int temp = 0; temp < (1 << 2 * cctvList.size()); temp++) {
-            for (int i = 0; i < X; i++)
-                System.arraycopy(board1[i], 0, board2[i], 0, Y);
-            int brute = temp;
-            for (Pair pair : cctvList) {
-                int dir = brute % 4;
-                brute /= 4;
-                monitor(pair.x, pair.y, dir);
-            }
+    // cctv 의 보는 방향은 4가지. 각 방향에 따라서, 탐색 범위를 정해야 한다.
+    // 7이상을 감시중인 것으로 표시한다.
+    static void go(int level, int start) {
+        if (start == cctvCnt) {
+            ans = Math.min(ans, blindSpots);
+            return;
         }
-        System.out.println(blindSpots);
+        int cctv = cctvList.get(start);
+        int cx = cctv / y, cy = cctv % y;
+        for(int dir = 0; dir < 4; dir++) {
+            monitor(cx ,cy, dir, 7);
+            arr[level] = dir;
+            go(level+1, start+1);
+            monitor(cx, cy, dir,0);
+        }
     }
-
-    static void monitor(int x, int y, int dir) {
-        switch (board1[x][y]) {
-            case 1 ->
-                upd(x, y, dir);
+    static void monitor(int cx, int cy, int dir, int state) {
+        switch (board[cx][cy]) {
+            case 1 -> bfs(cx, cy, state, dirArr[dir]);
             case 2 -> {
-                upd(x, y, dir);
-                upd(x, y, dir+2);
+                switch (dir) {
+                    case NORTH, SOUTH -> bfs(cx, cy, state, dirArr[NORTH], dirArr[SOUTH]);
+                    case EAST, WEST -> bfs(cx, cy, state, dirArr[EAST], dirArr[WEST]);
+                }
             }
-            case 3 -> {
-                upd(x, y, dir);
-                upd(x, y, dir+1);
-            }
+            case 3 -> bfs(cx, cy, state, dirArr[dir], dirArr[(dir+1)%4]);
             case 4 -> {
-                upd(x, y, dir);
-                upd(x, y, dir+1);
-                upd(x, y, dir+2);
+                switch (dir) {
+                    case NORTH -> bfs(cx, cy, state, dirArr[EAST], dirArr[NORTH], dirArr[WEST]);
+                    case EAST -> bfs(cx, cy, state, dirArr[NORTH], dirArr[EAST], dirArr[SOUTH]);
+                    case SOUTH -> bfs(cx, cy, state, dirArr[EAST], dirArr[SOUTH], dirArr[WEST]);
+                    case WEST -> bfs(cx, cy, state, dirArr[SOUTH], dirArr[WEST], dirArr[NORTH]);
+                }
             }
-            case 5 -> {
-                upd(x, y, dir);
-                upd(x, y, dir+1);
-                upd(x, y, dir+2);
-                upd(x, y, dir+3);
-            }
+            case 5 -> bfs(cx, cy, state, dirArr);
         }
-        int val = 0;
-        for (int i = 0; i < X; i++) {
-            for(int j = 0; j < Y; j++) {
-                if(board2[i][j] == 0) val++;
+    }
+
+    static void bfs(int cx, int cy, int state,  int[]... dirArr) {
+        for(int[] dir : dirArr) {
+            q.add(new Pair(cx, cy));
+            while (!q.isEmpty()) {
+                Pair cur = q.remove();
+                int nx = cur.x + dir[0];
+                int ny = cur.y + dir[1];
+                if(nx < 0 || ny < 0 || nx >= x || ny >= y) break;
+                if(board[nx][ny] == 6) break;
+                if (board[nx][ny] >= 1 && board[nx][ny] <= 5) {
+                    q.add(new Pair(nx, ny));
+                    continue;
+                }
+                if(state == 7) {
+                    if(board[nx][ny] == 0) {
+                        blindSpots--;
+                        board[nx][ny] = state;
+                    }
+                    else board[nx][ny]++;
+                }
+                else {
+                    if(board[nx][ny] == 7) {
+                        blindSpots++;
+                        board[nx][ny] = state;
+                    }
+                    else board[nx][ny]--;
+                }
+                q.add(new Pair(nx, ny));
             }
-        }
-        blindSpots = Math.min(blindSpots, val);
-    }
-    static boolean OOB(int nx, int ny) {
-        return nx < 0 || ny < 0 || nx >= X || ny >= Y;
-    }
-    static void upd(int x, int y, int dir) {
-        dir %= 4;
-        while(true) {
-            x += dx[dir];
-            y += dy[dir];
-            if(OOB(x, y) || board2[x][y] == 6) return;
-            if(board2[x][y] != 0) continue;
-            board2[x][y] = 7;
         }
     }
     static class Pair {
