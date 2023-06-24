1296d07290666d4f6bccbde29c162dcef5025fa5
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-06-24 12:45:55 +0900

    [Gold IV] Title: 뱀, Time: 160 ms, Memory: 17144 KB -BaekjoonHub

---
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int n, k, L, ans;
+    static char[][] board;
+    static final int[] dx = {0, 1, 0, -1};
+    static final int[] dy = {1, 0, -1, 0};
+    static final int RIGHT = 0, DOWN = 1, LEFT = 2, UP = 3;
+    static Deque<Pair> snake = new LinkedList<>();
+    static Queue<Pair> q = new LinkedList<>();
+    static boolean[] command;
+    static char[] dirArr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        board = new char[n][n];
+        k = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++)
+            Arrays.fill(board[i], '.');
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            board[Integer.parseInt(st.nextToken())-1][Integer.parseInt(st.nextToken())-1] = '@';
+        }
+        L = Integer.parseInt(br.readLine());
+        command = new boolean[10000];
+        dirArr = new char[L];
+        for (int i = 0; i < L; i++) {
+            st = new StringTokenizer(br.readLine());
+            int fir = Integer.parseInt(st.nextToken());
+            char two = st.nextToken().charAt(0);
+            command[fir] = true;
+            dirArr[i] = two;
+        }
+        snake.add(new Pair(0, 0));
+        board[0][0] = '$';
+        game();
+    }
+
+    static void game() {
+        boolean finish = false;
+        int dir = RIGHT;
+        int idx = 0;
+        while(!finish) {
+            ans++;
+            Pair last = null;
+            while(!snake.isEmpty()) {
+                Pair cur = snake.remove();
+                if(last == null) {
+                    int nx = cur.x + dx[dir];
+                    int ny = cur.y + dy[dir];
+                    if(nx < 0 || ny < 0 || nx >= n || ny >= n || board[nx][ny] == '$') {
+                        finish = true;
+                        break;
+                    }
+                    if(board[nx][ny] == '@') {
+                        snake.addFirst(cur);
+                        snake.addFirst(new Pair(nx, ny));
+                        board[nx][ny] = '$';
+                        break;
+                    }
+                    board[cur.x][cur.y] = '.';
+                    board[nx][ny] = '$';
+                    q.add(new Pair(nx, ny));
+                } else {
+                    board[cur.x][cur.y] = '.';
+                    board[last.x][last.y] = '$';
+                    q.add(last);
+                }
+                last = cur;
+            }
+            if(command[ans])
+                dir = setDir(dirArr[idx++], dir);
+            while(!finish && !q.isEmpty())
+                snake.add(q.remove());
+        }
+        System.out.println(ans);
+    }
+
+    static int setDir(char command, int dir) {
+        return (command == 'L') ? (dir + 3) & 3 : (dir + 1) & 3;
+    }
+
+    static class Pair {
+        int x, y;
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
