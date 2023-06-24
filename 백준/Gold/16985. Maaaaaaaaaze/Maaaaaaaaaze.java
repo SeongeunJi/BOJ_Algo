7a0af8147b1df76d09118c4c2c80c3629a0429d6
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-06-24 10:02:24 +0900

    [Gold II] Title: Maaaaaaaaaze, Time: 1396 ms, Memory: 297048 KB -BaekjoonHub

--- 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.java
+++ 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.java
@@ -1,60 +1,59 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int N = 5, ans = Integer.MAX_VALUE;
-    static boolean[] used = new boolean[N];
-    static int[] idxArr = new int[N];
-    static int[][][] maze = new int[N][N][N];
+    static int n = 5, ans = Integer.MAX_VALUE;
     static int[] dx = {1, 0, -1, 0, 0, 0};
     static int[] dy = {0, 1, 0, -1, 0, 0};
     static int[] dz = {0, 0, 0, 0, 1, -1};
+    static int[][][][] board = new int[4][n][n][n];
+    static int[][][] maze = new int[n][n][n];
+    static int[] orders = new int[n];
+    static boolean[] used = new boolean[n];
     static Queue<Tuple> q = new LinkedList<>();
-    static int[][][][] board = new int[4][N][N][N];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                for(int k = 0; k < N; k++) {
+                for (int k = 0; k < n; k++)
                     board[0][i][j][k] = Integer.parseInt(st.nextToken());
-                }
             }
-            for(int j = 0; j < N; j++)
-                for(int k = 0; k < N; k++)
-                    board[1][i][k][N-1-j] = board[0][i][j][k];
-            for(int j = 0; j < N; j++)
-                for(int k = 0; k < N; k++)
-                    board[2][i][k][N-1-j] = board[1][i][j][k];
-            for(int j = 0; j < N; j++)
-                for(int k = 0; k < N; k++)
-                    board[3][i][k][N-1-j] = board[2][i][j][k];
+            for(int j = 0; j < n; j++)
+                for(int k = 0; k < n; k++)
+                    board[1][i][k][n-1-j] = board[0][i][j][k];
+            for(int j = 0; j < n; j++)
+                for(int k = 0; k < n; k++)
+                    board[2][i][k][n-1-j] = board[1][i][j][k];
+            for(int j = 0; j < n; j++)
+                for(int k = 0; k < n; k++)
+                    board[3][i][k][n-1-j] = board[2][i][j][k];
         }
-        backtracking_idx(0);
+        go(0);
         System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
     }
 
-    static void backtracking_idx(int level) {
-        if(level == N) {
-            brute_dir();
+    static void go(int level) {
+        if(level == n) {
+            determine_dir();
             return;
         }
-        for(int i = 0; i < N; i++) {
+        for(int i = 0; i < n; i++) {
             if(used[i]) continue;
-            idxArr[level] = i;
             used[i] = true;
-            backtracking_idx(level+1);
+            orders[level] = i;
+            go(level + 1);
             used[i] = false;
         }
     }
 
-    static void brute_dir() {
-        for (int tmp = 0; tmp < (1 << 2 * N); tmp++) {
+    static void determine_dir() {
+        for(int tmp = 0; tmp < 1 << 10; tmp++) {
             int brute = tmp;
-            for (int i = 0; i < N; i++) {
-                int dir = brute % 4;
-                brute /= 4;
-                for (int j = 0; j < N; j++)
-                    System.arraycopy(board[dir][idxArr[i]][j], 0, maze[i][j], 0, N);
+            for(int i = 0; i < n; i++) {
+                int dir = brute & 3;
+                brute >>= 2;
+                for (int j = 0; j < n; j++)
+                    System.arraycopy(board[dir][orders[i]][j], 0, maze[i][j], 0, n);
             }
             ans = Math.min(ans, bfs());
             q.clear();
@@ -63,25 +62,25 @@ public class Main {
 
     static int bfs() {
         if(maze[0][0][0] == 0 || maze[4][4][4] == 0) return Integer.MAX_VALUE;
-        int[][][] dist = new int[N][N][N];
-        q.add(new Tuple(0, 0, 0));
+        int[][][] dist = new int[n][n][n];
         dist[0][0][0] = 1;
+        q.add(new Tuple(0, 0, 0));
         while (!q.isEmpty()) {
             Tuple cur = q.remove();
             for (int dir = 0; dir < 6; dir++) {
                 int nz = cur.z + dz[dir];
                 int nx = cur.x + dx[dir];
                 int ny = cur.y + dy[dir];
-                if(nx < 0 || ny < 0 || nz < 0 || nx >= N || ny >= N || nz >= N) continue;
+                if(nz < 0 || nx < 0 || ny < 0 || nz >= n || nx >= n || ny >= n) continue;
                 if(maze[nz][nx][ny] == 0 || dist[nz][nx][ny] != 0) continue;
                 if(nz == 4 && nx == 4 && ny == 4) return dist[cur.z][cur.x][cur.y];
-                q.add(new Tuple(nz, nx, ny));
                 dist[nz][nx][ny] = dist[cur.z][cur.x][cur.y] + 1;
+                q.add(new Tuple(nz, nx, ny));
             }
         }
-
         return Integer.MAX_VALUE;
     }
+
     static class Tuple {
         int z, x, y;
         public Tuple(int z, int x, int y) {
