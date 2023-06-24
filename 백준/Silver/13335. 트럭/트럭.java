20056000fe1baf69e764218549344a098af45054
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-06-24 09:21:39 +0900

    [Silver I] Title: 트럭, Time: 316 ms, Memory: 27788 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -1,45 +1,43 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int n, w, l, ans;
-    static Queue<Integer> q = new LinkedList<>();
     static int[] bridge;
+    static Queue<Integer> q = new LinkedList<>();
+    static int n, w, L, time;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         w = Integer.parseInt(st.nextToken());
-        l = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
         bridge = new int[w];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++)
             q.add(Integer.parseInt(st.nextToken()));
         do {
-            int tmp = calculate();
-            if (tmp <= l) {
-                tmp -= bridge[w - 1];
-                moveTrucks();
-                if (!q.isEmpty() && tmp + q.peek() <= l)
+            int curWeight = calWeight();
+            if (curWeight <= L) {
+                curWeight -= bridge[w-1];
+                move();
+                if(!q.isEmpty() && curWeight + q.peek() <= L)
                     bridge[0] = q.remove();
             }
-            ans++;
-        } while (!isEmpty());
-        System.out.println(ans);
+            time++;
+        } while (!isAllPassed());
+        System.out.println(time);
     }
-    static void moveTrucks() {
-        for(int i = w-1; i > 0; i--)
+
+    static int calWeight() {
+        return Arrays.stream(bridge).sum();
+    }
+
+    static boolean isAllPassed() {
+        return Arrays.stream(bridge).allMatch(e -> e == 0);
+    }
+
+    static void move() {
+        for (int i = w-1; i > 0; i--)
             bridge[i] = bridge[i-1];
         bridge[0] = 0;
-    }
-    static boolean isEmpty(){
-        for(int i = 0; i < w; ++i)
-            if(bridge[i] != 0) return false;
-        return true;
-    }
-    static int calculate(){
-        int sum = 0;
-        for(int i = 0; i<w; ++i)
-            sum+=bridge[i];
-        return sum;
     }
 }
