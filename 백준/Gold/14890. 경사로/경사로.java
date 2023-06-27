794fde7126d0a190d16837327c4ecf7113796958
Seongeun Ji <124786638+SeongeunJi@users.noreply.github.com>
2023-06-27 10:25:28 +0900

    [Gold III] Title: 경사로, Time: 156 ms, Memory: 15148 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -11,9 +11,8 @@ public class Main {
         board = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++) {
+            for(int j = 0; j < N; j++) 
                 board[i][j] = Integer.parseInt(st.nextToken());
-            }
         }
         int cnt = 0;
         for(int i = 0; i < N; i++) {
